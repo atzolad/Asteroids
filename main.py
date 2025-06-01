30ada4fa917bcf9ab84383960ee304f59cad6377
atzolad <atzolad@gmail.com>
2025-05-31 23:05:08 -0700

    Added player object and movement

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
 from constants import *
+from player import *
+#source venv/bin/activate
 
 def main():
+
+    #Initialize the game environment
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!" )
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         
+        #Allows the user to close out the window and end the game loop. 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
               
-        pygame.Surface.fill(screen, "#000000")
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        #Restricts the game FPS to 60 and returns the time in seconds.
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
